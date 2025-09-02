0a8244af0a2a0b3a82cf2dc7e89bf0bb138fa38b
GerardoBru <81394402+GerardoBru@users.noreply.github.com>
2025-09-01 20:33:58 -0500

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,59 @@
+import java.util.Sccaner;
+
+public class Menu{
+    public  static void main(string args[]){
+        Sccaner Sccaner = new Sccaner(system.in);
+     
+
+int opc = sccaner.nextint();
+string operacion= "";
+int menu = 0;
+
+system.out.println("porfavor ingrese su nombre: ");
+
+system.out.println("selecciones su combo: ");
+system.out.println("combo1: hamburguesa + papas fristas + gaseosa($20.000)");
+system.out.println("combo2: ensalada + jugo natural($15000)");
+system.out.println("sandwich + papas fritas + gaseosa($18.000)");
+system.out.println("pasta mas gaseosa($22.000)");
+int combo = sccaner.nextint();
+
+system.out.println("ingrese el combo que desea: ");
+
+
+system.out.println("seleccione una opcion adicional: ");
+system.out.println("1.agregar postre ($5.000)");
+system.out.println("2.agregar queso extra($3.000)");
+system.out.println("3.nunguna opcion adicional");
+int adicional = sccaner.nextint();
+
+system.out.println("ingrese el numero adicional que desea: ");
+
+system.out.println("seleccione el metodo de pago que desea: ");
+system.out.println("1.tarjeta de credito.");
+system.out.println("2.efectivo");
+int metodo = sccaner.nextInt();
+
+switch(opc) {
+    case 1 = 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+
+
+}
